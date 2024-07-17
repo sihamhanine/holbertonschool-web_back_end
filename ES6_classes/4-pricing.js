0c8425ad5e573e14cc6bbc359d2af6fe187af7b3
sihamelani <siham.elani17@hotmail.com>
2024-07-17 12:03:44 +0200

    Task4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,29 @@
+import Currency from './3-currency';
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency))
+        throw TypeError('currency must be an instance of Currency');
+    this._currency = newCurrency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
