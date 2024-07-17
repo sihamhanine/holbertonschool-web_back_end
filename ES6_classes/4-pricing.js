b76f14ee211764e2334af89ffc17068d75ed24c7
sihamelani <siham.elani17@hotmail.com>
2024-07-17 12:06:20 +0200

    Task4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -26,4 +26,8 @@ export default class Pricing {
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
