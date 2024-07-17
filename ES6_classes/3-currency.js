4edb6cb89be27d40b66f571aef9a7968ea5d163c
sihamelani <siham.elani17@hotmail.com>
2024-07-17 11:44:39 +0200

    Task3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  };
+
+  set code(newCode) {
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
