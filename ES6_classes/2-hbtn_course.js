78bcededeb095e3c5c5518dd9002e4397c1c9100
sihamelani <siham.elani17@hotmail.com>
2024-07-17 11:22:53 +0200

    Task2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students){
+    if (typeof name !== 'string') throw TypeError('name must be a string');
+    if (typeof length !== 'number') throw TypeError('length must be a Number');
+    if (!Array.isArray(students)) throw TypeError('students must be an array');
+    students.forEach((student) => {
+        if (typeof student !== 'string') throw TypeError('student must be a string');
+    });
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(newNme) {
+    if (typeof newName !== 'string') throw TypeError('neWName must be a string');
+    this._name = newName;
+  }
+
+  get name() {
+    return this._name;
+  }
+  
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw TypeError('newLength must be a number');
+    this._length = newLength;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) throw TypeError('students must be an array');
+    newStudents.forEach((student) => {
+      if (typeof student !== 'string') throw TypeError('student must be a string');
+    });
+    this._students = newStudents;
+  }
+  get students() {
+    return this._students;
+  }
+}
