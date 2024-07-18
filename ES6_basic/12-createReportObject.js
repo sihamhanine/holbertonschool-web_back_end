feb664099d0a7d75e1543ae3e07cfb5ab3dee0f7
sihamelani <siham.elani17@hotmail.com>
2024-07-18 11:19:23 +0200

    Task12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -3,9 +3,8 @@ export default function createReportObject(employeesList) {
     allEmployees: {
       ...employeesList,
     },
-      getNumberOfDepartments(employeesList){
-        return Object.keys(employeesList).length;
-      },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
-
 }
