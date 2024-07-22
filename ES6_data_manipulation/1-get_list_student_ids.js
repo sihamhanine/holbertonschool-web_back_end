841d69473ac18b25040c59e6bf3cb5065c664230
sihamelani <siham.elani17@hotmail.com>
2024-07-22 14:15:21 +0200

    Task1 correction

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
 export default function getListStudentIds(array1) {
-  let newArray1 = [];
+  const newArray1 = [];
   if (array1 instanceof Array) {
     return array1.map((item) => item.id);
   }
-
-  return newArray1;   
+  return newArray1;
 }
