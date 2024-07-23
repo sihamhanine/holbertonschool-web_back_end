b6ed6489bb8dbf920e990b26bd48b437f7df3653
sihamelani <siham.elani17@hotmail.com>
2024-07-23 12:38:52 +0200

    Task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,23 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
-}
+const handleProfileSignup = async (
+  firstName,
+  lastName,
+  fileName,
+) => {
+  const result = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    result.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+  return result;
+};
 
 export default handleProfileSignup;
