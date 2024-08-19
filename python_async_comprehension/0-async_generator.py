7bb4a35a77a73bed5cb1b0a34e2e80b0e6e2c5e5
sihamelani <siham.elani17@hotmail.com>
2024-08-19 09:21:34 +0200

    Task0 correction

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-""" Async comprhension Task0"""
-import Async, random
+"""Creates a generator"""
+import asyncio
+import random
+from typing import Generator
 
 
-async def async_generator():
-    """ coroutine called async_generator that takes no arguments.
-        The coroutine will loop 10 times, each time asynchronously
-        wait 1 second, then yield a random number between 0 and 10.
-    """
-    for i in range(10):
-        await async.sleep(1)
+async def async_generator() -> Generator[float, None, None]:
+    """Each time asynchronously waits 1 second,
+        then yield a random number between 0 and 10"""
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
