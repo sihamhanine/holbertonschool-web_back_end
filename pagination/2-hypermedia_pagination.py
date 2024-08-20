894f44b2e7aa9fe3f88fb7706fea9fc7d501ca4d
sihamelani <siham.elani17@hotmail.com>
2024-08-20 11:56:14 +0200

    Task2

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+""" hypermedia pagination """
+import math
+import csv
+from typing import List, Dict, Any
+
+
+def index_range(page, page_size)-> tuple:
+    """ function named index_range that takes two integer
+        arguments page and page_size.
+        The function should return a tuple of size two containing a
+        start index and an end index corresponding to the range of indexes
+        to return in a list for those particular pagination parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Exclut l'en-tête du fichier CSV
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retourne une page de données."""
+        assert isinstance(page, int) and page > 0, "Le numéro de page doit être un entier positif."
+        assert isinstance(page_size, int) and page_size > 0, "La taille de la page doit être un entier positif."
+        
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        
+        if start_index >= len(dataset):
+            return []
+        
+        return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Retourne des informations supplémentaires pour la pagination."""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
