3cf39ba0af3fcbb08361d28157b1f8f3dbd75fbc
sihamelani <siham.elani17@hotmail.com>
2024-09-17 19:52:03 +0200

    Task1 correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.setEncoding('utf8');
-
+console.log('Welcome to Holberton School, what is your name?');
+// await input
+process.stdin.resume();
+// once input is entered
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
   }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
 });
